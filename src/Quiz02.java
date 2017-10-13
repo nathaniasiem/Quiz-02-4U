1bd98133b0ee50bbdade2ed4094f65a95dc04c19
simon7323 <simon7323@HRH-IC0027344.wrdsb.ca>
2017-10-13 10:14:33 -0400

    13/10/2017-Recursion Quiz completed

--- src/Quiz02.java
+++ src/Quiz02.java
@@ -8,17 +8,18 @@
  * @author simon7323
  */
 public class Quiz02 {
-    public String reverseString(String word){
-     //create a base case
-        if(word.length()<=1){
-         return word;
-     }
-     //create a variable to 
-     String firstWord=word.substring(0,word.length());
-        System.out.println(firstWord);
-//     String endWord = word.substring(word.length(),0); 
-//        System.out.println(endWord);
-    return reverseString(word);
+
+    public String reverseString(String word) {
+        //create a base case
+        if (word.length() < 1) {
+            return word;
+        }
+        //create a variable to set the first index
+        String firstWord = word.substring(0, word.length() - 1);
+        //create a variable to set the ending index
+        String endWord = word.substring(word.length() - 1);
+        //call the method that will return the ending index in front and the beginning index to be at the end
+        return (endWord) + reverseString(firstWord);
     }
 
     /**
@@ -27,6 +28,11 @@ public class Quiz02 {
     public static void main(String[] args) {
         // create variable to test boundaries
         Quiz02 test = new Quiz02();
+        //call the method 
         System.out.println(test.reverseString("king"));
+        System.out.println("--------------------");
+        System.out.println(test.reverseString("car"));
+        System.out.println("--------------------");
+        System.out.println(test.reverseString("tree"));
     }
 }
